cac4bb3ae9823620c79863801d82f23345bbe81a
Kevin Chen <kchen@lcs.on.ca>
2025-03-03 14:22:31 -0500

    Initial Commit

---
+++ JokeFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JokeFinder
+//
+//  Created by Haowen Chen on 2025-03-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
