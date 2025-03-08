73537fdac8ecf09c66f8938c388f54435ede3d26
Kevin Chen <kchen@lcs.on.ca>
2025-03-08 03:02:11 -0500

    Can now delete saved jokes.

--- JokeFinder/Views/FavouriteJokesView.swift
+++ JokeFinder/Views/FavouriteJokesView.swift
@@ -44,6 +44,16 @@ struct FavouriteJokesView: View {
                                 Text(currentJoke.punchline ?? "")
                                     .italic()
                             }
+                            .swipeActions {
+                                
+                                // Delete
+                                Button("Delete", role: .destructive) {
+                                    withAnimation {
+                                        viewModel.delete(currentJoke)
+                                    }
+                                }
+                                
+                            }
                         }
                         .listStyle(.plain)
                         
